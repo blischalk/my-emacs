1a05303f03a2c054e97c9776e82ed3275f2fe9a8
Brett Lischalk <brett@brettlischalk.com>
2015-01-30 23:38:58 -0600

    Add php-mode and web-mode.

---
+++ elpa/web-mode-20150130.1122/web-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; web-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21708 25244
+;;;;;;  427829 792000))
+;;; Generated autoloads from web-mode.el
+
+(autoload 'web-mode "web-mode" "\
+Major mode for editing web templates.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("web-mode-pkg.el") (21708 25244 435018
+;;;;;;  802000))
+
+;;;***
+
+(provide 'web-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; web-mode-autoloads.el ends here
